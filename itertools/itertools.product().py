a23bb188ce52fe0a7e960c3f210abed40b64ddb5
andrewliu0732 <95197467+andrewliu0732@users.noreply.github.com>
2023-11-11 20:58:04 -0800

    Create itertools.product().py
    https://www.hackerrank.com/challenges/itertools-product

---
+++ itertools/itertools.product().py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/itertools-product 
+
+from itertools import product
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+print(*list(product(A, B)))
